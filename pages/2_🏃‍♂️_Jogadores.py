4eb651677a23cfcf4fe21783d7d00f4c487f95f4
ABeuthner <beuthner1@gmail.com>
2025-09-28 20:13:38 -0300

    atualizando o codigo

--- pages/2_🏃‍♂️_Jogadores.py
+++ pages/2_🏃‍♂️_Jogadores.py
@@ -11,11 +11,10 @@ players = sorted(df_playes["Name"].dropna().unique())
 player = st.sidebar.selectbox("Jogadores", players)
 
 player_stats = df[df["Name"] == player].iloc[0]
-player_stats
 
 
 Jogador = st.markdown(f'# {player_stats["Name"]}')
-player_stats.loc["Photo"]
+st.markdown(f'[Photo do jogador]({player_stats.loc["Photo"]})')
 st.markdown(f"**Clube:** {player_stats.loc['Club']}")
 st.markdown(f"**Posision:** {player_stats.loc['Position']}")
 
@@ -24,6 +23,7 @@ col1, col2, col3 = st.columns(3)
 col1.markdown(f"**Idade:** {player_stats.loc['Age']}")
 col2.markdown(f"**Altura:** {player_stats.loc['Height(cm.)']/100}")
 col3.markdown(f"**Peso:** {player_stats.loc['Weight(lbs.)']*0.453:.2f}")
+st.divider()
 
 st.subheader(f"Overall: {player_stats.loc['Overall']}")
 st.progress(int(player_stats.loc['Overall']))
