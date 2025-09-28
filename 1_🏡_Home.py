a31a921be6fa4f1328bd9e7597852229270f48ec
ABeuthner <beuthner1@gmail.com>
2025-09-28 01:08:12 -0300

    inciando os codigos

---
+++ 1_🏡_Home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+# Configurando a pagina
+
+st.set_page_config(
+    layout='centered'
+
+)
+
+df = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv")
+
+st.title("FIFA 2023 ⚽!")
+st.link_button(
+    label="Acesse a base de dados no Kaggle",
+    url="https://www.kaggle.com/datasets",
+    type="primary"
+)
+
