95d150c700be86a28450b84a3a5d830aa8739e24
ABeuthner <beuthner1@gmail.com>
2025-09-28 20:05:22 -0300

    atualizando a segunda pagina com os dados dos jogadores selecionados

--- pages/2_🏃‍♂️_Jogadores.py
+++ pages/2_🏃‍♂️_Jogadores.py
@@ -1,4 +1,31 @@
 import streamlit as st
+import pandas as pd
 
 df = st.session_state["data"]
-df
+
+clubes = sorted(df["Club"].dropna().unique())
+club = st.sidebar.selectbox("Clube", clubes)
+
+df_playes = df[(df["Club"] == club)]
+players = sorted(df_playes["Name"].dropna().unique())
+player = st.sidebar.selectbox("Jogadores", players)
+
+player_stats = df[df["Name"] == player].iloc[0]
+player_stats
+
+
+Jogador = st.markdown(f'# {player_stats["Name"]}')
+player_stats.loc["Photo"]
+st.markdown(f"**Clube:** {player_stats.loc['Club']}")
+st.markdown(f"**Posision:** {player_stats.loc['Position']}")
+
+col1, col2, col3 = st.columns(3)
+
+col1.markdown(f"**Idade:** {player_stats.loc['Age']}")
+col2.markdown(f"**Altura:** {player_stats.loc['Height(cm.)']/100}")
+col3.markdown(f"**Peso:** {player_stats.loc['Weight(lbs.)']*0.453:.2f}")
+
+st.subheader(f"Overall: {player_stats.loc['Overall']}")
+st.progress(int(player_stats.loc['Overall']))
+
+
