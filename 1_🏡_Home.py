7e0e4223c8a7827f811756393e61eb716a03db37
ABeuthner <beuthner1@gmail.com>
2025-09-28 01:26:25 -0300

    atualizando código e finalizando a página 1

--- 1_🏡_Home.py
+++ 1_🏡_Home.py
@@ -8,13 +8,32 @@ st.set_page_config(
 
 )
 
-df = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv")
-df
+st.markdown("# FIFA 2023 ⚽ - OFICIAL!")
+st.sidebar.markdown("Desenvovimento: Amanda Beuthner")
 
-st.title("FIFA 2023 ⚽!")
+
 st.link_button(
     label="Acesse a base de dados no Kaggle",
     url="https://www.kaggle.com/datasets",
     type="primary"
 )
 
+df = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv")
+df
+
+st.markdown(
+    """
+    O conjunto de dados
+    de jogadores de futebol de 2017 a 2023 fornece informações 
+    abrangentes sobre jogadores de futebol profissionais.
+    O conjunto de dados contém uma ampla gama de atributos, incluindo dados demográficos 
+    do jogador, características físicas, estatísticas de jogo, detalhes do contrato e 
+    afiliações de clubes. 
+    
+    Com **mais de 17.000 registros**, este conjunto de dados oferece um recurso valioso para 
+    analistas de futebol, pesquisadores e entusiastas interessados em explorar vários 
+    aspectos do mundo do futebol, pois permite estudar atributos de jogadores, métricas de 
+    desempenho, avaliação de mercado, análise de clubes, posicionamento de jogadores e 
+    desenvolvimento do jogador ao longo do tempo.
+"""
+)
